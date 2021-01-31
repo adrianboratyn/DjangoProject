2747f69280e92291b7cae34121510fd32b552cc0
Kamilgolda <kamil.goldaat@gmail.com>
2021-01-31 23:31:41 +0100

    Strona z wycieczkami (ListView)

--- DjangoProject/home/urls.py
+++ DjangoProject/home/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from .views import TripsListView
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('oferty/', TripsListView.as_view(), name='wycieczki'),
 ]
--- DjangoProject/home/views.py
+++ DjangoProject/home/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+from .models import Trip
+from django.views.generic import ListView
+
 
 def home(request):
     return render(request, 'home/home.html')
+
+
+class TripsListView(ListView):
+    model = Trip
+    template_name = 'home/trips.html'
+    context_object_name = 'trips'
+    ordering = ['date_posted']
+
+
+def wycieczki(request):
+    context = {
+        'trips': Trip.objects.all()
+    }
+    return render(request, 'home/trips.html', context)

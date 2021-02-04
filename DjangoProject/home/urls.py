0bbce8466f4e95add7a8f3134c1c0373cf34ad8c
Kamilgolda <kamil.goldaat@gmail.com>
2021-02-04 01:35:41 +0100

    strona rezerwacji wycieczki (render partial)

--- DjangoProject/home/urls.py
+++ DjangoProject/home/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('oferty/', TripsListView.as_view(), name='wycieczki'),
     path('oferty/<int:pk>/', TripsDetailView.as_view(), name='trips-detail'),
+    path('oferty/<int:pk>/succes', views.reservation, name='reservation'),
+
 ]

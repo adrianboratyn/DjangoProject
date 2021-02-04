b0a5c02582a16d2fefae4d3e4cbcf3da9d5a3764
Kamilgolda <kamil.goldaat@gmail.com>
2021-02-04 01:37:01 +0100

    update JS rezerwacji wycieczki

--- DjangoProject/home/static/home/js/reservation.js
+++ DjangoProject/home/static/home/js/reservation.js
@@ -1,23 +1,41 @@
+    var userk= document.getElementById("k2").textContent
+    var tripk= document.getElementById("k1").textContent
+    var user= document.getElementById("id_user")
+    var trip= document.getElementById("id_trip")
+    var termink= document.getElementById("termin")
+    var termin= document.getElementById("id_day")
+    var cenak= document.getElementById("k3").textContent
+    var dorosli= document.getElementById("id_adults")
+    var dzieci=document.getElementById("id_kids")
+    var cena= document.getElementById("id_price")
+    var przewodnik=document.getElementById("id_guide")
+    var pokoj=document.getElementById("id_room")
+
+    dorosli.oninput = getPrice;
+    dzieci.oninput = getPrice;
+    przewodnik.oninput = getPrice;
+    pokoj.oninput = getPrice;
+    termink.onchange = getPrice;
+
+getPrice();
 function getPrice()
 {
-    console.log(1)
     
-    var cena= parseInt(document.getElementById("price").innerText);
-    var dorosli= parseInt(document.getElementById("dorosli").value);
-    var dzieci=parseInt(document.getElementById("dzieci").value)
-    var result= dorosli*cena+dzieci*(cena*0.75)
-    console.log(result)
+    user.value=userk
+    trip.value=tripk
+    termin.value=termink.options[termink.selectedIndex].value
+    var result= parseInt(dorosli.value)*cenak+(parseInt(dzieci.value)*(cenak*0.75))
 
-var przewodnik=document.getElementById("prywatny_przewodnik")
-if(przewodnik.checked)  
-    result += 300
-var pokoj=document.getElementById("pokoj")
-if(pokoj.checked)
-    result += 400
+    if(przewodnik.checked)  
+        result += 300
 
-var out=document.getElementById("out")
+    if(pokoj.checked)
+        result += 400
+    
+        cena.value=result
 
+    var out=document.getElementById("out")
 
-out.innerText = result
+    out.innerText = result
 }
 

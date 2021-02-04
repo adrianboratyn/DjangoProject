5eb1f46e76ac1a0cd65444ebe5cd5b0a255ebe81
Kamilgolda <kamil.goldaat@gmail.com>
2021-02-04 01:30:39 +0100

    Model rezerwacji wycieczki

--- DjangoProject/home/models.py
+++ DjangoProject/home/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-from datetime import date
 
 
 class Trip(models.Model):
@@ -21,5 +20,17 @@ class Trip(models.Model):
     image_four = models.ImageField(upload_to='trips_pics', blank=True)
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+
+
+class Reservation(models.Model):
+    user = models.TextField(blank=True)
+    trip = models.TextField(blank=True)
+    day = models.TextField(blank=True)
+    adults = models.DecimalField(max_digits=2, decimal_places=0, default=1)
+    kids = models.DecimalField(max_digits=2, decimal_places=0, default=0)
+    phone = models.DecimalField(max_digits=9, decimal_places=0)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    guide = models.BooleanField(default=0)
+    room = models.BooleanField(default=0)
 
 

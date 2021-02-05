4732559e1bf03d6ccc98fae9e80ebc674d020139
Michasssss <michaeldzindzio@gmail.com>
2021-02-06 00:04:29 +0100

    Contact Page

--- DjangoProject/home/urls.py
+++ DjangoProject/home/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import TripsListView, TripsDetailView
+from .views import TripsListView, TripsDetailView,Contact
 from . import views
-from .views import HomeView, ExchangeRatesView
+from .views import HomeView, ExchangeRatesView,ContactView
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
@@ -10,5 +10,5 @@ urlpatterns = [
     path('oferty/<int:pk>/', TripsDetailView.as_view(), name='trips-detail'),
     path('oferty/<int:pk>/succes', views.reservation, name='reservation'),
     path('waluty/', ExchangeRatesView.as_view(), name="exchangerates"),
-
+    path('kontakt/', views.Contact, name="contact"),
 ]

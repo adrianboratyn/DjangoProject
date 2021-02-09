47d6ff8d1153dc42285ad8c80dc818b5246f320d
Kamilgolda <kamil.goldaat@gmail.com>
2021-02-09 21:09:13 +0100

    Dodanie wycieczki

--- DjangoProject/home/static/home/js/weather.js
+++ DjangoProject/home/static/home/js/weather.js
@@ -1,4 +1,5 @@
-var z = {Ponta_Delgada: "37d74n25d68/ponta-delgada/"}
+var z = {Ponta_Delgada: "37d74n25d68/ponta-delgada/",
+         Dominican_Republic: "18d74n70d16/dominican-republic/"}
 
 var pogoda = document.getElementById('pogoda')
 pogoda.href += z[pogoda.getAttribute('data-label_1')] 
